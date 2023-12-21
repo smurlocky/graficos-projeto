990e2bb7640300ad6410304b9103db370a2e3812
murlock <mvinicius.avelino@hotmail.com>
2023-12-21 19:22:17 -0300

    versiona projeto graficos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,60 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NgChartsModule } from 'ng2-charts';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgChartsModule],
   template: `
     <h1>Welcome to {{title}}!</h1>
-
+    <div>
+      <canvas id="line-chart"></canvas>
+    </div>
     <router-outlet></router-outlet>
   `,
   styles: [],
 })
 export class AppComponent {
   title = 'graficos-projeto';
+  graph = {};
+
+  ngOnInit() {
+    const ctx = document.getElementById("line-chart") as HTMLCanvasElement;
+    if (ctx) {
+      this.graph = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+        datasets: [  {
+          label: 'Vendas',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          
+          hoverBorderWidth: 5,
+          borderWidth: 1
+        }
+        ]
+      }
+    },
+  )}
+    
+  }
 }
